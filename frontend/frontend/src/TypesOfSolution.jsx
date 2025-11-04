856995a376c0d2ca589b535b83f96a37c19416a7
Tasoshammer <tasoshammer@gmail.com>
2025-11-04 15:12:28 +0200

    Creating TypesOfSolution element

---
+++ frontend/frontend/src/TypesOfSolution.jsx
@@ -0,0 +1,15 @@
+export default function TypesOfSolution(){
+    return (
+        <>
+            <select>
+                    <option value="someOption">Some option</option>
+                    <option value="otherOption">Other option</option>
+            </select>
+
+
+        </>
+
+
+    );
+
+}
