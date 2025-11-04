0f5e30c57665e50781ff3f92eb71c4663130f538
Tasoshammer <tasoshammer@gmail.com>
2025-11-04 17:13:41 +0200

    file uploader

--- frontend/frontend/src/TypesOfSolution.jsx
+++ frontend/frontend/src/TypesOfSolution.jsx
@@ -1,24 +1,35 @@
+import React, { useState } from "react";
 import './TypesOfSolution.css';
 
-export default function TypesOfSolution(){
-    return (
-        <div id="body-types-of-solution">
-            
-            <div id="div-select">
-            <p id="p-type">Type of the Solution</p>
-            <select id="select-type">
-                    <option value="WriteCodeHere">Write Code Here</option>
-                    <option value="UploadFile">Upload File</option>
-            </select>
-            
-            </div>
+export default function TypesOfSolution() {
+  const [file, setFile] = useState(null);
 
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
+  const handleUpload = () => {
+    if (!file) return;
+    console.log("Ανεβάζουμε:", file);
+  };
 
+  return (
+    <div id="body-types-of-solution">
+     
+      <div id="div-select">
+        <p id="p-type">Type of the Solution</p>
+        <select id="select-type">
+          <option value="WriteCodeHere">Write Code Here</option>
+          <option value="UploadFile">Upload File</option>
+        </select>
+      </div>
 
-        </div>
-
-
-    );
-
+     
+         <div id="file-uploader">
+        <input type="file" onChange={handleFileChange} />
+        {file && <p>Επιλεγμένο αρχείο: {file.name}</p>}
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+    </div>
+  );
 }
