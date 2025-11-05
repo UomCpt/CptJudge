3225c3d9719f3f2f4293eb62307b5a22348c5ca8
Tasoshammer <tasoshammer@gmail.com>
2025-11-05 11:29:12 +0200

    Upload file when selected

--- frontend/frontend/src/TypesOfSolution.jsx
+++ frontend/frontend/src/TypesOfSolution.jsx
@@ -4,6 +4,15 @@ import './TypesOfSolution.css';
 export default function TypesOfSolution() {
   const [file, setFile] = useState(null);
 
+  const [selected, setSelected] = useState("WriteCodeHere");
+
+  
+  const handleSelect = (event)=>{
+    setSelected(event.target.value);
+    
+  }
+  
+  
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
@@ -13,10 +22,12 @@ export default function TypesOfSolution() {
     console.log("Ανεβάζουμε:", file);
   };
 
+
+
   return (
     <div id="body-types-of-solution">
      
-      <div id="div-select">
+      <div id="div-select" onChange={handleSelect}>
         <p id="p-type">Type of the Solution</p>
         <select id="select-type">
           <option value="WriteCodeHere">Write Code Here</option>
@@ -24,12 +35,13 @@ export default function TypesOfSolution() {
         </select>
       </div>
 
-     
+        {selected=="UploadFile" && 
+
          <div id="file-uploader">
         <input type="file" onChange={handleFileChange} />
         {file && <p>Επιλεγμένο αρχείο: {file.name}</p>}
         <button onClick={handleUpload}>Upload</button>
-      </div>
+      </div>}
     </div>
   );
 }
