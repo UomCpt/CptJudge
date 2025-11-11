8f869f83bb72bf853428d52fdf4187da06e2c7a7
Tasoshammer <tasoshammer@gmail.com>
2025-11-11 12:59:58 +0200

    className center

--- frontend/frontend/src/TypesOfSolution.jsx
+++ frontend/frontend/src/TypesOfSolution.jsx
@@ -33,22 +33,44 @@ export default function TypesOfSolution() {
   return (
     <div id="body-types-of-solution">
      
-      <div id="div-select" onChange={handleSelect}>
+      <div id="div-select"  onChange={handleSelect}>
         <p id="p-type">Type of the Solution</p>
-        <select id="select-type">
+        <select id="select-type" >
           <option value="WriteCodeHere">Write Code Here</option>
           <option value="UploadFile">Upload File</option>
         </select>
       </div>
 
+        {selected=="WriteCodeHere"&& 
+        <div id="page-editor" className="center"> 
+          <p>ksdaskldaklsndfnf lngjrfgnjng jn  gkmjrtkhkrthn </p>
+          <input type="text" />
+          
+
+          
+          
+          
+
+
+        </div>
+        
+
+        }
+        
+        
+        
+        
+        
+        
         {selected=="UploadFile" && 
 
-         <div id="file-uploader">
+         <div id="file-uploader" className="center">
         {!uploaded &&<input type="file" onChange={handleFileChange} />}
         { !uploaded&& file && <p>Selected File: {file.name}</p>}
         {!uploaded && <button onClick={handleUpload}>Submit</button>}
         {uploaded && <p>🟢 You have succesfully uploaded the file: {file.name}</p>}
       </div>}
+      
     </div>
   );
 }
